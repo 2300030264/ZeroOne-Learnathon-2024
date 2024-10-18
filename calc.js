0b53fb5ac062106627137a9f6b0653bcca232686
2300030264 <163744552+2300030264@users.noreply.github.com>
2024-10-18 13:01:50 +0530

    Add files via upload
    2300030264 - JAKKA VARSHA SREE

---
+++ calc.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+let a =20;
+let b =50;
+function div(a,b){
+    return a/b;
+}
+console.log(sub(a,b));
+console.log(add(15,67));
+console.log(sub(25,50));
